e2b9f562da44c6171a92fb797a7aef438c4bb78b
Vishwajeet Singh <rvishwajeet9829@gmail.com>
2023-05-16 10:06:25 +0530

    commmit

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const express= require('express')
+const express = require("express");
+const app = express();
+let bodyParser = require("body-parser");
 
-const app= express()
-app.listen('3000',()=>{console.log("Listening at 3000")})
+app.use(bodyParser.json());
+app.get("/hello-world", (req, res) => {
+  res.send("Hello");
+});
+
+app.post("/new-request", (req, res) => {
+  res.send(req.body);
+});
+
+app.listen("3000", () => {
+  console.log("Listening at 3000");
+});
